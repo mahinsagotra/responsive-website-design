5228bdaf5d521d403b0ea5d27bed6d1ada3812fe
Mahin Sagotra <mahinsagotra18@outlook.com>
2021-02-04 15:21:37 +0530

    Hero component Complete

--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../App.css";
-import { Button } from "../Button/Button";
+import Button from "../Button/Button";
 import "./HeroSection.css";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/public/videos/video-2.mp4" autoPlay loop muted />
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
